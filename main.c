a708354cd6b335db5f12f8b180cd7b28446a4bbe
naimkim <naimkim2000@naver.com>
2019-11-12 15:43:58 +0900

    03_project

--- main.c
+++ main.c
@@ -5,20 +5,17 @@
 
 int main(void) {
 	
-	char*pc;
-	int *pi;
+	char buffer[8];
 	double *pd;
+	int *pi;
+	pd = (double *)buffer;
+	*pd = 3.14;
+	printf("%f\n", *(double*)buffer );
+	pi = (int *)buffer;
+	*pi = 123;
+	*(pi+1) = 456;
+	printf("%d %d\n", *(int*)buffer, *((int*)buffer + 1) );  
 	
-	pc = (char *)10000;
-	pi = (int *)10000;
-	pd = (double *)10000;
-	
-	printf("증가 전 : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
-	
-	pc++;
-	pi++;
-	pd++;
-	printf("증가 후 : pc = %d, pi = %d,, pd =%d\n",pc, pi, pd);
 	
 	return 0;
 }
